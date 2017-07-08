608337af45ad4bb84c6686ef660cb829ffda6bca
Minh Dinh <dtminhpt@gmail.com>
2017-07-08 13:42:01 +0700

    Show DrawerNavigator

--- index.ios.js
+++ index.ios.js
@@ -12,26 +12,6 @@ import {
   View
 } from 'react-native';
 
-import App from './app/index.js'
-
-AppRegistry.registerComponent('TwitterAssign3', () => App);
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import App from './app/index1.js'
 
 AppRegistry.registerComponent('Assign3', () => App);
